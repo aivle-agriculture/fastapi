d52b2d1e83ca6152640a03fe88f743df44948cb0
ddanglehee <shee9812@naver.com>
2025-06-19 19:52:53 +0900

    chore: 잘못 추가된 주석 삭제

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from app.api.chatbot_api import router as chatbot_router
+from app.api.chatbot_api import router as chatbot_router
 from app.api.calculate_api import router as calculate_router
 import os
 from dotenv import load_dotenv
@@ -8,5 +8,5 @@ load_dotenv()
 
 app = FastAPI()
 
-# app.include_router(chatbot_router, prefix="/api")
+app.include_router(chatbot_router, prefix="/api")
 app.include_router(calculate_router, prefix="/api")
